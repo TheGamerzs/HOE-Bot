70f3788889388b727ac5a8bbd95b129c2b7e750e
Declan Mould <44209534+TheGamerzs@users.noreply.github.com>
2022-11-18 01:36:00 +0000

    Delete dist directory

--- dist/events/ready.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.run = exports.name = void 0;
-exports.name = "ready";
-const run = (bot) => {
-    console.log(`Logged in as ${bot?.user?.tag}`);
-};
-exports.run = run;
